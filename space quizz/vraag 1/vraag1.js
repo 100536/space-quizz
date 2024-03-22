08dff2b32caf80afff4de2c1194da1284132994d
LucasA <153184467+100536@users.noreply.github.com>
2024-03-22 10:48:21 +0100

    Add files via upload

---
+++ space quizz/vraag 1/vraag1.js
@@ -0,0 +1,15 @@
+function showGoodPopup() {
+    document.getElementById('popup-container-good').style.display = 'block';
+}
+
+function showBadPopup() {
+    document.getElementById('popup-container-bad').style.display = 'block';
+}
+
+function closeGoodPopup() {
+    document.getElementById('popup-container-good').style.display = 'none';
+}
+
+function closeBadPopup() {
+    document.getElementById('popup-container-bad').style.display = 'none';
+}
